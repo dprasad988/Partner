9c8c967919da9e3403a60c09a931a6fbdd367303
dprasad988 <154485871+dprasad988@users.noreply.github.com>
2024-03-29 22:57:31 +0530

    updated signin

--- frontend/src/pages/signin.js
+++ frontend/src/pages/signin.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import Header from '../components/header'
-import axios from 'axios'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Header from '../components/header';
 
-export default function Signin() {
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="http://localhost:3000">
+        PartnerPro
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
+const defaultTheme = createTheme();
+
+export default function SignIn() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: '',
     password: ''
   });
-
-  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,61 +45,95 @@ export default function Signin() {
       [name]: value
     }));
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Perform validation if needed
 
-     // Send POST request to sign-in endpoint
-     axios.post('http://localhost:5005/signin', formData)
-     .then(res => {
-       const { token } = res.data;
-       // Store JWT token securely (e.g., in local storage)
-       localStorage.setItem('jwtToken', token);
-       console.log('JWT token stored:', token);
-       // Navigate to user's profile page or dashboard upon successful sign-in
-       navigate('/profile');
-     })
-     .catch(err => {
-      // Handle errors here
-      if (err.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.error("Server responded with status code:", err.response.status);
-        console.error("Error message from server:", err.response.data);
-      } else if (err.request) {
-        // The request was made but no response was received
-        console.error("No response received from server");
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.error("Error while sending request:", err.message);
-      }
-    });
- };
-  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:5005/signin', formData)
+      .then(res => {
+        const { token } = res.data;
+        localStorage.setItem('jwtToken', token);
+        navigate('/profile');
+      })
+      .catch(err => {
+        console.error("Error:", err);
+      });
+  };
+
   return (
     <div>
-      <Header />
-    <div className='container'>
-      <div className='row justify-content-center '>
-      
-      <div className='row-6 col-6 border border-3 p-3 mt-3 '>
-        <form onSubmit={handleSubmit}>
-          <h1 className=' m-0 text-center '>Sign In Here</h1>
-            <div className='col-md-12'>
-                <label className='form-label'>Email</label>
-                <input className='form-control' type='email' name='email' placeholder='Enter Your Email' value={formData.email} onChange={handleChange}></input>
-            </div>
-            <div className='col-md-12'>
-                <label className='form-label'>Password</label>
-                <input className='form-control' type='password' name='password' placeholder='Enter Your Password' value={formData.password} onChange={handleChange}></input>
-            </div>
-            <div className='col-md-12 mt-2'>
-                <button className='btn btn-primary'>Sign In</button>
-            </div>
-          </form>  
-      </div>
-      </div>
+        <Header />
+    
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
     </div>
-    </div>
-  )
+  );
 }
--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,189 +1,213 @@
-import React from 'react'
-import Header from '../components/header';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { RadioGroup } from '@mui/material';
+import { Radio } from '@mui/material';
+import { FormControl, FormLabel } from '@mui/material';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { validateEmail, validatePassword } from '../components/functions.js';
+import Header from '../components/header.js';
 
-export default function Signup() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        contact: '',
-        gender: '',
-        goal: '',
-        pass: '',
-        confpass: ''
-      });
-    
-    const navigate = useNavigate();
+const defaultTheme = createTheme();
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
+export default function SignUp() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    contact: '',
+    gender: '',
+    goal: '',
+    pass: '',
+    confpass: ''
+  });
 
-    const handleSubmit = (e) => {
-        e.preventDefault(); // Prevent default form submission behavior
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
 
-        if (!validateEmail(formData.email)) {
-            alert('Please enter a valid email address.');
-            return;
-        }
-        if (!validatePassword(formData.pass)) {
-            alert('Password must be at least 8 characters long.');
-            return;
-        }
-        if (formData.pass !== formData.confpass) {
-            alert('Passwords do not match.');
-            return;
-        }
-    
-        axios.post('http://localhost:5005/signup', formData) // Send POST request to backend
-        .then(res => {
-            console.log(res); // Log response data to the console
-            if (res.status === 200) {
-                alert('Registered successfully');
-                navigate('/signin'); // Navigate to sign-in page if registration is successful
-            }
-        })
-        .catch(err => console.log(err)); // Log any errors to the console
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
+
+    if (!validateEmail(formData.email)) {
+      alert('Please enter a valid email address.');
+      return;
     }
-    
+    if (!validatePassword(formData.pass)) {
+      alert('Password must be at least 8 characters long.');
+      return;
+    }
+    if (formData.pass !== formData.confpass) {
+      alert('Passwords do not match.');
+      return;
+    }
+
+    axios.post('http://localhost:5005/signup', formData) // Send POST request to backend
+      .then(res => {
+        console.log(res); // Log response data to the console
+        if (res.status === 200) {
+          alert('Registered successfully');
+          navigate('/signin'); // Navigate to sign-in page if registration is successful
+        }
+      })
+      .catch(err => console.log(err)); // Log any errors to the console
+  }
 
   return (
     <div>
-        <Header/>
-        <div className="container d-flex justify-content-center align-items-center mt-2 ">
-  <div className="card shadow-lg rounded-3 p-4">
-    <div className="card-body">
-      <h1 className="card-title text-center mb-4">Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            className="form-control rounded-pill"
-            id="name"
-            name="name"
-            placeholder="Enter your name"
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            className="form-control rounded-pill"
-            id="email"
-            name="email"
-            placeholder="Enter your email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="contact" className="form-label">
-            Contact Number
-          </label>
-          <input
-            type="text"
-            className="form-control rounded-pill"
-            id="contact"
-            name="contact"
-            placeholder="Enter your mobile number"
-            value={formData.contact}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Gender</label>
-          <div>
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="gender"
-                id="male"
-                value="Male"
-                onChange={handleChange}
-              />
-              <label className="form-check-label" htmlFor="male">
-                Male
-              </label>
-            </div>
-            <div className="form-check form-check-inline">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="gender"
-                id="female"
-                value="Female"
-                onChange={handleChange}
-              />
-              <label className="form-check-label" htmlFor="female">
-                Female
-              </label>
-            </div>
-          </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="goal" className="form-label">
-            What's Your Goal
-          </label>
-          <input
-            type="text"
-            className="form-control rounded-pill"
-            id="goal"
-            name="goal"
-            placeholder="Enter your goal"
-            value={formData.goal}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="pass" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control rounded-pill"
-            id="pass"
-            name="pass"
-            placeholder="Enter your password"
-            value={formData.pass}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="confpass" className="form-label">
-            Re-Enter Password
-          </label>
-          <input
-            type="password"
-            className="form-control rounded-pill"
-            id="confpass"
-            name="confpass"
-            placeholder="Re-enter your password"
-            value={formData.confpass}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary btn-lg w-100 rounded-pill">
-          Submit
-        </button>
-      </form>
+      < Header />
+    
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="name"
+                  name="name"
+                  required
+                  fullWidth
+                  id="name"
+                  label="Name"
+                  autoFocus
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="email"
+                  name="email"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="tel"
+                  name="contact"
+                  required
+                  fullWidth
+                  id="contact"
+                  label="Contact Number"
+                  value={formData.contact}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">Gender</FormLabel>
+                  <RadioGroup
+                    aria-label="gender"
+                    name="gender"
+                    value={formData.gender}
+                    onChange={handleChange}
+                  >
+                    <FormControlLabel value="male" control={<Radio />} label="Male" />
+                    <FormControlLabel value="female" control={<Radio />} label="Female" />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="off"
+                  name="goal"
+                  required
+                  fullWidth
+                  id="goal"
+                  label="Goal"
+                  value={formData.goal}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="new-password"
+                  name="pass"
+                  required
+                  fullWidth
+                  id="pass"
+                  label="Password"
+                  type="password"
+                  value={formData.pass}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="new-password"
+                  name="confpass"
+                  required
+                  fullWidth
+                  id="confpass"
+                  label="Confirm Password"
+                  type="password"
+                  value={formData.confpass}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
     </div>
-  </div>
-</div>
-    </div>
-  )
+  );
 }
